290abfe355235f07884166f4d4463d4a97a11115
sohail.salem@diracsystems.com <Sohail@123>
2024-06-12 02:45:13 +0300

    jar

--- src/main/java/com/example/demo/KeyWord/KeyWordRepository.java
+++ src/main/java/com/example/demo/KeyWord/KeyWordRepository.java
@@ -1,12 +1,13 @@
 package com.example.demo.KeyWord;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface KeyWordRepository extends JpaRepository<KeyWord, Integer> {
-//    @Query("SELECT k from KeyWord k where k.text = ?1 AND k.tutorial_Id = ?2")
+  @Query("SELECT k from KeyWord k where k.text = ?1 AND k.tutorialId = ?2")
     Optional<KeyWord> getByTextAndTutorialId(String text, int tutorialId);
     Optional<KeyWord> findByLevel(String level);
     Optional<KeyWord> findByTutorialId(int tutorialId);
