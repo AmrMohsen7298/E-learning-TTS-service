2745abca32910dbba439a5e6d3c4f583ffdf450e
AmrMohsen7298 <amr.mohsen7298@gmail.com>
2024-07-30 00:25:41 +0300

    add cors

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,11 +6,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(
+		origins = {"https://tts.eliteacademyeg.com","https://ttsdash.eliteacademyeg.com","*"}
+
+)
 
 @SpringBootApplication
 @EnableConfigurationProperties({
 		FileStorageProperties.class
 })
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
